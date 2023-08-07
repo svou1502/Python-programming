76611b68de2aa0ef3123e62926ec876e1ee0a44a
Admin <Admin@STUDENT07>
2023-08-07 12:34:53 +0100

    Steven's code - Lab1

---
+++ Casting variables.py
@@ -0,0 +1,17 @@
+# Input the length of the first side of the rectangle
+length = int(input("Enter the length of the first side of the rectangle: "))
+
+# Input the length of the second side of the rectangle
+width = int(input("Enter the length of the second side of the rectangle: "))
+
+# Calculate the perimeter of the rectangle
+perimeter = 2 * (length + width)
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Display the calculated perimeter and area
+print("Perimeter:", perimeter)
+print("Area:", area)
+
+
---
+++ Lab1.py
@@ -0,0 +1,16 @@
+print ('Hello World')
+
+username='Bob' 
+age=32 
+print(username,'is',age,'years old')
+
+username = 'Bob'
+age = 32
+print(username + 'is' + str(age) + 'years old')
+
+username = 'Bob'
+age = 32
+print(username + ' is ' + str(age) + ' years old')
+
+
+
