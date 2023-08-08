4ebb4fa17cbd79b110235f1b69bc216ce4a67e73
Admin <Admin@STUDENT07>
2023-08-08 12:20:57 +0100

    Part 3 - Investment

--- Steven - Lab 3/Squares.py
+++ Steven - Lab 3/Squares.py
@@ -2,7 +2,7 @@ number = 1
 
 while number <= 100:
     square = number * number
-    print(f"Number: {number}, Square: {square}")
+    print("Number:", (str)(number), "Square:", str(square))
     
     if square > 2000:
         break
