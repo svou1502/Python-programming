eba600962cebded2375f960d6ae7c54f17724d73
Admin <Admin@STUDENT07>
2023-08-08 14:47:18 +0100

    Part 2 - Count Vowels

--- Steven - Lab 3/Count vowels.py
+++ Steven - Lab 3/Count vowels.py
@@ -1,1 +1,9 @@
+word = input("Enter a word: ") # Ask user to input
+vowel_count = 0
+
+for char in word:
+    if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+        vowel_count += 1
+
+print(f"The word '{word}' contains {vowel_count} vowels.")
 
