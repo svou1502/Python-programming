da39faad5c980912c9b2c6f802db82b7559d2789
Admin <Admin@STUDENT07>
2023-08-11 15:14:36 +0100

    Lab 11 - Part 1

--- Steven - Lab 8/Part 1 - Create table.py
+++ Steven - Lab 8/Part 1 - Create table.py
@@ -1,1 +1,27 @@
+import pyodbc
 
+connectionString = r'DRIVER={ODBC Driver 13 for SQL Server};SERVER=.\SQLExpress;DATABASE=qastore;Trusted_Connection=yes'
+
+sqlStr = """CREATE TABLE Student (
+    StudentID INT NOT NULL,
+    FirstName nvarchar(40) NOT NULL,
+    Surname nvarchar(30) NULL,
+    Course nvarchar(30) NULL,
+    City nvarchar(15) NULL)"""
+
+conn = pyodbc.connect(connectionString)
+cur = conn.cursor()
+cur.execute(sqlStr)
+
+conn.commit() 
+conn.close()
+
+sqlStr="""
+IF EXISTS(
+    SELECT TABLE_NAME, TABLE_SCHEMA
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_NAME = 'Student'
+    AND TABLE_SCHEMA = 'dbo')
+BEGIN
+    DROP TABLE Student
+END"""
